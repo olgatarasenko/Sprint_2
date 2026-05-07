2c9354b6cdbbbdfd39cdd6b8076b897a6158292e
Olga QA <olgaa.bereza@yandex.ru>
2026-05-07 09:55:37 +0300

    4 task fix

--- Sprint_2/task_4.py
+++ Sprint_2/task_4.py
@@ -8,13 +8,13 @@ class EmployeeSalary:
         self.email = email
 
     @classmethod
-    def get_hours(cls, name, hours, rest_days, email):
+    def get_hours(cls, name, hours=None, rest_days=None, email=None):
         if hours is None:
             hours = (7 - rest_days) * 8
         return cls(name, hours, rest_days, email)
 
     @classmethod
-    def get_email(cls, name, hours, rest_days, email):
+    def get_email(cls, name, hours=None, rest_days=None, email=None):
         if email is None:
             email = f"{name}@email.com"
         return cls(name, hours, rest_days, email)
