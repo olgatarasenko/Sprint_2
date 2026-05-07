bfa0fa4fb68d255ea955d08cc1de6c160cfc88ba
Olga QA <olgaa.bereza@yandex.ru>
2026-05-08 00:16:40 +0300

    2 sprint fix 4 task

--- Sprint_2/task_4.py
+++ Sprint_2/task_4.py
@@ -1,25 +1,27 @@
 class EmployeeSalary:
     hourly_payment = 400
 
-    def __init__(self, name, hours=None, rest_days=None, email=None):
+    def __init__(self, name, hours = 0, rest_days = 0, email = ''):
         self.name = name
         self.hours = hours
         self.rest_days = rest_days
         self.email = email
 
-    def get_hours(self):
-        if self.hours is None:
-            return (7 - self.rest_days) * 8
-        return self.hours
+    @classmethod
+    def get_hours(cls, name, hours = 0, rest_days = 0, email = ''):
+        if hours == 0:
+            hours = (7 - rest_days) * 8
+        return cls(name, hours, rest_days, email)
 
-    def get_email(self):
-        if self.email is None:
-            return f"{self.name}@email.com"
-        return self.email
+    @classmethod
+    def get_email(cls, name, hours = 0, rest_days = 0, email = ''):
+        if email == '':
+            email = f"{name}@email.com"
+        return cls(name, hours, rest_days, email)
 
     @classmethod
     def set_hourly_payment(cls, new_payment):
         cls.hourly_payment = new_payment
 
     def salary(self):
-        return self.get_hours() * self.hourly_payment
+        return self.hours * self.hourly_payment
